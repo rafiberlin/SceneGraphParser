8c172051ebae58fd7cf0044b7ef0707155b8f08c
rafi <rafiderberliner@googlemail.com>
2021-12-20 13:54:54 +0100

    Introduces the Variable DATASETS.TO_TEST. We can run a test on an dataset split by using train, test or val as value in the command line.

--- example/demo.py
+++ example/demo.py
@@ -22,21 +22,21 @@ def demo(sentence):
     # Here we just use the default parser.
     sng_parser.tprint(sng_parser.parse(sentence), show_entities=False)
 
-    print()
 
 
 def main():
-    demo('A woman is playing the piano in the room.')
-    demo('A woman playing the piano in the room.')
-    demo('A piano is played by a woman in the room.')
-    demo('A woman is playing the space craft at NASA.')
-    demo('A woman is playing with a space craft at NASA.')
-    demo('A woman next to a piano.')
-    demo('A woman in front of a piano.')
-    demo('A woman standing next to a piano.')
-    demo('The woman is a pianist.')
-    demo('A giraffe grazing a tree in the wildness with other wildlife.')
-    demo('Cow standing on sidewalk in city area near shops.')
+    demo('A white city bus pulling up to a bus stop.')
+    # demo('A woman is playing the piano in the room.')
+    # demo('A woman playing the piano in the room.')
+    # demo('A piano is played by a woman in the room.')
+    # demo('A woman is playing the space craft at NASA.')
+    # demo('A woman is playing with a space craft at NASA.')
+    # demo('A woman next to a piano.')
+    # demo('A woman in front of a piano.')
+    # demo('A woman standing next to a piano.')
+    # demo('The woman is a pianist.')
+    # demo('A giraffe grazing a tree in the wildness with other wildlife.')
+    # demo('Cow standing on sidewalk in city area near shops.')
 
 if __name__ == '__main__':
     main()
